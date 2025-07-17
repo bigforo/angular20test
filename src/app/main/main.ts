abbb392635ba24c034794932081d22a0e3fb702b
bigforo <foro@foro.mk>
2025-07-18 07:43:10 +1000

    Calendar working

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -13,4 +13,37 @@ import { JsonPipe } from '@angular/common';
 export class Main {
   service = inject(Common);
 
+  protected readonly Date = Date;
+
+  formatTimeHHMMSS(date: Date): string {
+    const hours = `${date.getHours()}`.padStart(2, '0');
+    const minutes = `${date.getMinutes()}`.padStart(2, '0');
+    const seconds = `${date.getSeconds()}`.padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
+  }
+  addMinutes(date: Date, minutes: number): Date {
+    return new Date(date.getTime() + minutes * 60000);
+  }
+  public getTime(){
+    const now = new Date();
+    return this.formatTimeHHMMSS(now);
+  }
+  public getTimeLater(){
+    const now = new Date();
+    return this.formatTimeHHMMSS(this.addMinutes(now, 30));
+  }
+  timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  public getDesc(){
+    let ex = this.service.appState.daily.exercises;
+    let desc:string = "";
+    ex.forEach(item => {
+      desc += item.name + '\r'
+      item.sets.forEach(set => {
+        desc += ' - ' + set.reps + 'x' + set.size + '\r'
+      })
+    })
+    return desc;
+  }
 }
