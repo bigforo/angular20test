ddd024f2dfecccc5308bf36657889e49311787ce
bigforo <foro@foro.mk>
2025-07-27 23:24:22 +1000

    F1 SPA Ohrid
    horisontal scroll

--- src/app/classes/exercise.class.ts
+++ src/app/classes/exercise.class.ts
@@ -9,8 +9,8 @@ export interface IExercise{
 export class ExerciseClass implements IExercise {
   public name: string;
   public sets: ExerciseSetClass[];
-  public reps: number[] = [8,9,10,11,12,13,14,15];
-  public weight: number[] = [20,25,30,35,40,45,50,55];
+  public reps: number[] = [8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+  public weight: number[] = [10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100];
 
   constructor(name: string) {
     this.name = name;
