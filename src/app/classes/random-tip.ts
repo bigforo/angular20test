0ebbb310088db45319d0b14f6d70ab770f5bcc27
bigforo <foro@foro.mk>
2025-11-01 22:43:21 +1100

    tips

--- src/app/classes/random-tip.ts
+++ src/app/classes/random-tip.ts
@@ -7,9 +7,14 @@ import {BehaviorSubject, Observable, of, Subject} from 'rxjs';
 export class RandomTip {
 
   private quotes = [
-    { text: 'Click on the Exercise to add activity', number: 1 },
-    { text: 'Swipe left to delete/remove Exercise', number: 2 },
-    { text: 'The future belongs to those who believe in the beauty of their dreams.', number: 3 }
+    { text: 'Click on the Exercise to add activity.', number: 1 },
+    { text: 'Swipe left to delete/remove Exercise.', number: 2 },
+    { text: 'The future belongs to those who believe in the beauty of their dreams.', number: 3 },
+    { text: 'Don’t count the days, make the days count.', number: 4},
+    { text: 'The body achieves what the mind believes.', number: 5},
+    { text: 'When you feel like quitting, think about why you started.', number: 6},
+    { text: 'Sweat is just fat crying.', number: 7},
+    { text: 'It never gets easier, you just get stronger.', number: 8},
   ];
 
   constructor() { }
