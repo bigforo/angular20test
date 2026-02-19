70f9acdf2c4361d206e85b2175707e8b678221c9
bigforo <foro@foro.mk>
2026-02-19 14:52:46 +1100

    add cal

--- src/app/pages/session/session-details.ts
+++ src/app/pages/session/session-details.ts
@@ -1,21 +1,20 @@
-import {Component, inject, input, linkedSignal, OnInit} from '@angular/core';
-import {DailySummary} from "../../components/daily-summary/daily-summary";
+import {Component, CUSTOM_ELEMENTS_SCHEMA, inject, input, linkedSignal, OnInit} from '@angular/core';
 import {CommonService} from '../../classes/common.service';
 import {
-  IonBackButton, IonButton,
+  IonButton,
   IonButtons,
-  IonContent, IonFabButton, IonFooter,
-  IonHeader, IonIcon, IonItem, IonItemOption, IonLabel,
+  IonContent, IonFooter,
+  IonHeader, IonIcon,
   IonTitle,
-  IonToolbar, PopoverController
+  IonToolbar
 } from '@ionic/angular/standalone';
 import {NavigationEnd, Router, RouterLink} from '@angular/router';
 import {LocalStorageService} from '../../classes/ls';
 import {addIcons} from 'ionicons';
 import {
-  arrowBack, arrowBackOutline,
+  arrowBackOutline,
+  calendarNumberOutline,
   cloudDownload,
-  copy,
   ellipsisHorizontal,
   ellipsisVertical,
   share,
@@ -24,10 +23,8 @@ import {
   starOutline
 } from 'ionicons/icons';
 import {WorkoutDetails} from "../../components/workout-details/workout-details";
-import {PopoverPage} from '../sessions/about-popover';
-import {Session} from '../../classes/state.interface';
-import {DatePipe} from '@angular/common';
-import {filter, take} from 'rxjs';
+import {Activity, Session} from '../../classes/state.interface';
+import {filter} from 'rxjs';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
@@ -38,20 +35,15 @@ import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
     IonTitle,
     IonToolbar,
     IonContent,
-    IonBackButton,
     IonButton,
     IonIcon,
     RouterLink,
     WorkoutDetails,
     IonFooter,
-    IonFabButton,
-    DatePipe,
-    IonItem,
-    IonLabel,
-    IonItemOption
   ],
   templateUrl: './session-details.html',
-  styleUrl: './session-details.scss'
+  styleUrl: './session-details.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SessionDetails implements OnInit{
   service = inject(CommonService);
@@ -89,6 +81,7 @@ export class SessionDetails implements OnInit{
       ellipsisHorizontal,
       ellipsisVertical,
       arrowBackOutline,
+      calendarNumberOutline,
     });
   }
 
@@ -99,28 +92,90 @@ export class SessionDetails implements OnInit{
 
 
 
-  shareLink() {
+  // shareLink() {
+  //
+  //   const currentUrl = new URL(window.location.href);
+  //   currentUrl.pathname = '/view';
+  //   currentUrl.searchParams.set('id', this.generatedLink()??"");
+  //   const shareUrl = currentUrl.toString();
+  //
+  //   const shareData = {
+  //     title: 'Gym Mate',
+  //     text: 'Check my workout!',
+  //     url: shareUrl,
+  //   };
+  //
+  //   if (navigator.share) {
+  //     navigator.share(shareData)
+  //       .then(() => console.log('Link shared successfully'))
+  //       .catch((error) => console.error('Error sharing:', error));
+  //   } else {
+  //     // fallback: copy link to clipboard
+  //     navigator.clipboard.writeText(shareData.url).then(() => {
+  //       alert('Link copied to clipboard!');
+  //     });
+  //   }
+  // }
 
-    const currentUrl = new URL(window.location.href);
-    currentUrl.pathname = '/view';
-    currentUrl.searchParams.set('id', this.generatedLink()??"");
-    const shareUrl = currentUrl.toString();
+  formatTimeHHMMSS(date: Date): string {
+    const hours = `${date.getHours()}`.padStart(2, '0');
+    const minutes = `${date.getMinutes()}`.padStart(2, '0');
+    const seconds = `${date.getSeconds()}`.padStart(2, '0');
 
-    const shareData = {
-      title: 'Gym Mate',
-      text: 'Check my workout!',
-      url: shareUrl,
-    };
+    return `${hours}:${minutes}:${seconds}`;
+  }
+  addMinutes(date: Date, minutes: number): Date {
+    return new Date(date.getTime() + minutes * 60000);
+  }
+  public getDate(){
+    const sessionStartDate = new Date (this.session().created);
+    return sessionStartDate.toISOString().split('T')[0];
+  }
+  public getTime(){
+    const sessionStartDate = new Date (this.session().created);
+    return this.formatTimeHHMMSS(sessionStartDate);
+  }
+  public getTimeLater(){
+    const sessionStartDate = new Date (this.session().created);
+    return this.formatTimeHHMMSS(this.addMinutes(sessionStartDate, 60));
+  }
+  timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-    if (navigator.share) {
-      navigator.share(shareData)
-        .then(() => console.log('Link shared successfully'))
-        .catch((error) => console.error('Error sharing:', error));
-    } else {
-      // fallback: copy link to clipboard
-      navigator.clipboard.writeText(shareData.url).then(() => {
-        alert('Link copied to clipboard!');
-      });
-    }
+
+  public getCalDesc() {
+    let exercises = this.session().activities;
+    let desc:string = "";
+    if (this.session().type)
+      desc += '[strong][u]' + this.session().type?.toUpperCase() +' SESSION[/u][/strong]'
+    else
+      desc += '[strong][u]GYM SESSION[/u][/strong]'
+
+    exercises?.forEach(activity => {
+      let exercise = Activity.exerciseById(activity.id)
+      desc += '[p]'
+      desc += '[strong]'+exercise?.name?.toUpperCase() +'[/strong] \r'
+      desc += '[ol]'
+      activity.sets.forEach(set => {
+        if (set.size)
+          desc += '[li]' + set.reps + ' x ' + set.size + ' kg [/li]'
+        else
+          desc += '[li]' + set.reps + '[/li]'
+      })
+      desc += '[/ol]'
+      if (activity.note)
+        desc += '[p] - '+activity.note.toLowerCase() +'[/p]'
+      desc += '[/p]'
+      desc += '[hr]'
+    })
+    if (this.session().note)
+      desc += '[p]NOTE: '+this.session().note?.toLowerCase() +'[/p][br]'
+    desc += '[p][url]https://gym.foro.mk/view?id='+this.generatedLink()+'|View in Gym Mate![/url][/p]'
+    return desc;
+  }
+  public getCalName() {
+    if (this.session().type)
+      return this.session().type?.toUpperCase() +' SESSION'
+    else
+      return 'GYM SESSION'
   }
 }
