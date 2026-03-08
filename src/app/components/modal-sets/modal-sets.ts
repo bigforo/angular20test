d5a664c55b1cc71b8716b6bc004a88ae4d9d297e
bigforo <foro@foro.mk>
2026-03-08 14:53:20 +1100

    sets fix

--- src/app/components/modal-sets/modal-sets.ts
+++ src/app/components/modal-sets/modal-sets.ts
@@ -1,107 +1,76 @@
-import {Component, effect, inject, input, linkedSignal, model, OnInit, output} from '@angular/core';
-import {
-  IonButton,
-  IonButtons,
-  IonContent,
-  IonHeader,
-  IonLabel,
-  IonInput,
-  IonModal, IonTextarea, IonTitle, IonToolbar, IonGrid, IonCol, IonRow, NavController
-} from "@ionic/angular/standalone";
-import {DatePipe} from '@angular/common';
-import {ExHistory} from '../ex-history/ex-history';
-import {ShowSets} from '../../pages/page-show-exercise/show-sets/show-sets';
-import {Activity} from '../../classes/state.interface';
-import {SetClass} from '../../classes/set.class';
-import {CommonService} from '../../classes/common.service';
-import {OverlayEventDetail} from '@ionic/core';
-import {FormsModule} from '@angular/forms';
-import {Router} from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { Component, effect, inject, input, linkedSignal, model, OnInit, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonLabel, IonModal, IonRow, IonTextarea, IonTitle, IonToolbar, NavController } from '@ionic/angular/standalone';
+import { OverlayEventDetail } from '@ionic/core';
+import { CommonService } from '../../classes/common.service';
+import { SetClass } from '../../classes/set.class';
+import { Activity } from '../../classes/state.interface';
+import { ShowSets } from '../../pages/page-show-exercise/show-sets/show-sets';
+import { ExHistory } from '../ex-history/ex-history';
 
 @Component({
   selector: 'app-modal-sets',
-  imports: [
-    IonButton,
-    IonButtons,
-    IonContent,
-    IonHeader,
-    IonLabel,
-    IonModal,
-    IonTitle,
-    IonToolbar,
-    DatePipe,
-    ExHistory,
-    ShowSets,
-    IonTextarea,
-    FormsModule,
-    IonInput,
-    IonGrid,
-    IonCol,
-    IonRow
-],
+  imports: [IonButton, IonButtons, IonContent, IonHeader, IonLabel, IonModal, IonTitle, IonToolbar, DatePipe, ExHistory, ShowSets, IonTextarea, FormsModule, IonInput, IonGrid, IonCol, IonRow],
   templateUrl: './modal-sets.html',
-  styleUrl: './modal-sets.scss'
+  styleUrl: './modal-sets.scss',
 })
-export class ModalSets implements OnInit{
-  service = inject(CommonService)
-  activity =  input.required<Activity>();
+export class ModalSets implements OnInit {
+  service = inject(CommonService);
+  activity = input.required<Activity>();
   open = input.required<boolean>();
   router = inject(Router);
   closed = output<boolean>();
   values = {
-    reps: "",
-    kgs: "",
-    free: "",
-  }
+    reps: '',
+    kgs: '',
+  };
   constructor() {
     effect(() => {
       //Default value when OPEN
-       this.values = {
-         reps: this.lastSet()?.reps ?? "",
-         kgs: this.lastSet()?.size ?? "",
-         free: this.note(),
-       }
+      this.values = {
+        reps: this.lastSet()?.reps ?? '',
+        kgs: this.lastSet()?.size ?? '',
+      };
     });
   }
-  note = linkedSignal(()=>{
+  note = linkedSignal(() => {
     return this.activity().note;
   });
-  lastSet =  linkedSignal(()=>{
+  lastSet = linkedSignal(() => {
     return this.activity().sets.at(-1);
-  })
+  });
   ngOnInit(): void {
     this.service.getAllHistoryByActivityId(this.activity().id);
   }
-  addSet(p0: Activity, values: {
-    reps: string
-    kgs: string
-    free: string
-  }) {
+  addSet( p0: Activity, values: { reps: string; kgs: string; } ) {
     this.service.startSessionIfNotStarted();
     let ex = Activity.exerciseById(p0.id);
-      if (ex?.weightUnit && ex?.reps)
-        this.activity().sets.push(new SetClass(values.reps, values.kgs));
-      else if  (!ex?.weightUnit && ex?.reps)
-        this.activity().sets.push(new SetClass(values.reps, ""));
-      else
-        this.activity().sets.push(new SetClass(values.free, ""));
 
-      this.service.save();
+    if (values.kgs !== '') {
+      this.activity().sets.push(new SetClass(values.reps, values.kgs));
+    }
+    else if (values.reps) {
+      this.activity().sets.push(new SetClass(values.reps, ''));
+    }
+
+    this.service.save();
   }
 
   async canDismiss(data?: undefined, role?: string) {
-    return role !== 'gesture' ;//&& role !== 'backdrop';
+    return role !== 'gesture'; //&& role !== 'backdrop';
   }
   onWillDismiss(event: CustomEvent<OverlayEventDetail>) {
     this.closed.emit(true);
     this.service.save();
   }
 
-  getNote():string {
-    return this.note()
+  getNote(): string {
+    return this.note();
   }
 
-  setNote(value:string) {
+  setNote(value: string) {
     this.activity().note = value;
     this.service.save();
   }
@@ -111,15 +80,15 @@ export class ModalSets implements OnInit{
   navyController = inject(NavController);
   redirect(modal: IonModal) {
     modal.isOpen = false;
-    void this.navyController.navigateForward("exercise/" +this.activity().id)
+    void this.navyController.navigateForward('exercise/' + this.activity().id);
   }
 
   protected readonly model = model;
 
-  clickToNavigate(id: number, modal:IonModal) {
+  clickToNavigate(id: number, modal: IonModal) {
     if (id > -1) {
       modal.isOpen = false;
-      void this.router.navigate(["session/"], {queryParams: {id: id}});
+      void this.router.navigate(['session/'], { queryParams: { id: id } });
     }
   }
 }
