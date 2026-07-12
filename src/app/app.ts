4b9b214bd4eb04e410265c01a9c60a0f2dcd4d84
bigforo <foro@foro.mk>
2026-07-12 19:39:18 +1000

    Native test

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,76 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { IonApp, IonContent, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonMenu, IonMenuToggle, IonRouterOutlet, IonSplitPane, IonToggle } from '@ionic/angular/standalone';
+import {
+  IonApp,
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenu,
+  IonMenuToggle,
+  IonRouterOutlet,
+  IonSplitPane,
+  IonToggle,
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { accessibilityOutline, calendarOutline, hammer, help, informationCircleOutline, logIn, logoBuffer, logOut, mapOutline, moonOutline, peopleOutline, person, personAdd, radioButtonOn, walk, walkOutline } from 'ionicons/icons';
+import {
+  accessibilityOutline,
+  calendarOutline,
+  hammer,
+  help,
+  informationCircleOutline,
+  logIn,
+  logoBuffer,
+  logOut,
+  mapOutline,
+  moonOutline,
+  peopleOutline,
+  person,
+  personAdd,
+  radioButtonOn,
+  walk,
+  walkOutline,
+} from 'ionicons/icons';
 import { VERSION } from '../environments/version';
 import { CommonService } from './classes/common.service';
 
+interface NativeAppMessage {
+  action: string;
+  value: string;
+}
+
+interface WebKitWindow extends Window {
+  webkit?: {
+    messageHandlers?: {
+      nativeApp?: {
+        postMessage(message: NativeAppMessage): void;
+      };
+    };
+  };
+}
+
 @Component({
   selector: 'app-root',
-  imports: [IonApp, IonRouterOutlet, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonMenuToggle, IonItem, IonLabel, IonToggle, FormsModule, IonIcon, RouterLink, RouterLinkActive],
+  imports: [
+    IonApp,
+    IonRouterOutlet,
+    IonSplitPane,
+    IonMenu,
+    IonContent,
+    IonList,
+    IonListHeader,
+    IonMenuToggle,
+    IonItem,
+    IonLabel,
+    IonToggle,
+    FormsModule,
+    IonIcon,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
@@ -55,60 +116,20 @@ export class App implements OnInit {
   // toast = inject(ToastController);
   _snackBar = inject(MatSnackBar);
 
-  // async presets(preset: string) {
-  //   if (!this.service.appState().current != null && ((this.service.appState().current?.activities?.length??0) > 0))
-  //   {
-  //     await this.router.navigate(['/app/tabs/current']);
-  //
-  //     this._snackBar.open("Can't add! Stop current workout session or swipe left to remove exercise!", "Close",{
-  //       duration: 5000,
-  //       verticalPosition: "top"
-  //     });
-  //     return;
-  //   }
-  //   else {
-  //     const ses = new Session("session");
-  //     ses.type = preset;
-  //     this.service.appState().current = ses;
-  //
-  //
-  //     if (preset === 'Chest day') {
-  //       EXERCISES.forEach(ele => {
-  //         if (
-  //           ele.id === 'b1' ||
-  //           ele.id === 'b2' ||
-  //           ele.id === 'b3'
-  //         ) {
-  //           const act = new Activity(ele);
-  //           this.service.appState().current?.activities.push(act);
-  //         }
-  //       });
-  //     } else if (preset === 'Back day') {
-  //       EXERCISES.forEach(ele => {
-  //         if (
-  //           ele.id === 'bk1' ||
-  //           ele.id === 'bk2' ||
-  //           ele.id === 'bk3'
-  //         ) {
-  //           const act = new Activity(ele);
-  //           this.service.appState().current?.activities.push(act);
-  //         }
-  //       });
-  //     } else if (preset === 'Shoulders day'){
-  //       EXERCISES.forEach(ele => {
-  //         if (
-  //           ele.id === 'ab1' ||
-  //           ele.id === 'ab2'
-  //         ) {
-  //           const act = new Activity(ele);
-  //           this.service.appState().current?.activities.push(act);
-  //         }
-  //       });
-  //     }
-  //     await this.router.navigate(['/app/tabs/current']);
-  //   }
-  //
-  // }
-  //
-  // protected readonly allExercises = EXERCISES;
+  sendSwift() {
+    const message: NativeAppMessage = {
+      action: 'openMenu',
+      value: 'hello',
+    };
+    const nativeApp = (window as WebKitWindow).webkit?.messageHandlers?.nativeApp;
+
+    if (nativeApp) {
+      nativeApp.postMessage(message);
+      return;
+    }
+
+    this._snackBar.open('Native iOS WebView bridge is not available.', 'Close', {
+      duration: 3000,
+    });
+  }
 }
